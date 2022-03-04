18a2b620c6c53e9b5b3349c26ffae4363c2b5559
Nafisa06 <nafisa.ila@gmail.com>
2022-03-05 00:24:51 +1100

    inserted roles into the table, with job title, salary and department id

--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,15 @@ VALUE ("Sales"),
  ("Engineering"),
  ("Finance"),
  ("Legal");
+
+ INSERT INTO roles (title, salary, department_id)
+VALUE 
+ ("Salesperson", 80000, 1)
+ ("Lead Engineer", 150000, 2),
+ ("Software Engineer", 120000, 2),
+ ("Account Manager", 160000, 3),
+ ("Accountant", 125000, 3),
+ ("Legal Team Lead", 250000, 4),
+ ("Lawyer", 190000, 4);
+
+ 
