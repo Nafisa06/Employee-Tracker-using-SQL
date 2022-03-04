9c24dd69afdf3617c1aade24fc8a915448f11ac7
Nafisa06 <nafisa.ila@gmail.com>
2022-03-04 23:44:41 +1100

    created table for roles

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,21 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
+--table for departments--
+
 CREATE TABLE departments (
   id INTEGER NOT NULL auto_increment PRIMARY KEY,
   name VARCHAR(30)
 );
 
+--table for roles--
+CREATE TABLE roles (
+  id INTEGER NOT NULL auto_increment PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+  ON DELETE SET NULL
+);
 
-
+--table for departments--
